a50efe3422dc500c0d8a2e11d39e93fd01a7b47a
Kevin Smith <kevin@operable.io>
2016-01-06 13:58:43 -0500

    Added goon formula

---
+++ Formula/goon.rb
@@ -0,0 +1,17 @@
+class Goon < Formula
+  desc "Middleman implementation for Porcelain"
+  homepage "https://github.com/alco/goon"
+  url "https://github.com/alco/goon/releases/download/v1.1.1/goon_darwin_amd64.tar.gz"
+  version "1.1.1"
+  sha256 "e5ff3e52c6ce11c4da96064addd6e7f1d9ff9e21953cb83fc430d28741a061f2"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    bin.install "goon"
+  end
+
+  test do
+    `system "#{bin}/goon", "--version"`
+  end
+
+end
