a5693b6f5034c86a826884917c3f5cfa00134ae9
Kevin Smith <kevin@operable.io>
2016-01-06 16:48:55 -0500

    Sorted out installation order

--- Formula/cog-dev.rb
+++ Formula/cog-dev.rb
@@ -2,10 +2,13 @@ class CogDev < Formula
   desc "Cog development environment meta-formula"
   url "https://github.com/operable/homebrew-operable"
   version "1.0"
+  sha256 "09a8f2877e568efc2a437bad0edeecc7f655bc5a35aaf6498d89143892fbec5f"
 
-  depends_on "automake"
-  depends_on "libtool"
   depends_on "erlang" => ["with-dirty-schedulers", "without-wxmac"]
-  depends_on "elixir"
   depends_on "libsodium"
+
+  def install
+    `/usr/local/bin/brew install elixir`
+  end
+
 end
