670e682da73fc1a3a8ed2ff622b87be5b30e5219
Kevin Smith <kevin@operable.io>
2016-01-06 16:29:57 -0500

    Manually set correct dep ordering

--- Formula/cog-dev.rb
+++ Formula/cog-dev.rb
@@ -3,7 +3,9 @@ class CogDev < Formula
   url "https://github.com/operable/homebrew-operable"
   version "1.0"
 
-  depends_on "erlang" => "with-dirty-schedulers"
+  depends_on "automake"
+  depends_on "libtool"
+  depends_on "erlang" => ["with-dirty-schedulers", "without-wxmac"]
   depends_on "elixir"
   depends_on "libsodium"
 end
