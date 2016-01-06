e2685a1146c09528fccd9bdee9b2afc824d0b8ca
Kevin Smith <kevin@operable.io>
2016-01-06 16:27:22 -0500

    Added required url attribute

--- Formula/cog-dev.rb
+++ Formula/cog-dev.rb
@@ -1,5 +1,6 @@
 class CogDev < Formula
   desc "Cog development environment meta-formula"
+  url "https://github.com/operable/homebrew-operable"
   version "1.0"
 
   depends_on "erlang" => "with-dirty-schedulers"
