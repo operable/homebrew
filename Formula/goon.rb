f3728ed2ca69b1667ff9f256b28a4523f3a96fce
Kevin Smith <kevin@operable.io>
2016-01-06 17:00:37 -0500

    Fixed `brew audit` warnings

--- Formula/goon.rb
+++ Formula/goon.rb
@@ -1,7 +1,7 @@
 class Goon < Formula
   desc "Middleman implementation for Porcelain"
   homepage "https://github.com/alco/goon"
-  url "https://github.com/alco/goon/releases/download/v#{version}/goon_darwin_amd64.tar.gz"
+  url "https://github.com/alco/goon/releases/download/v1.1.1/goon_darwin_amd64.tar.gz"
   version "1.1.1"
   sha256 "e5ff3e52c6ce11c4da96064addd6e7f1d9ff9e21953cb83fc430d28741a061f2"
 
