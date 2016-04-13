f7194000f98f469efca33d4ae87dd187bdbca192
Kevin Smith <kevin@operable.io>
2016-04-13 14:04:19 -0400

    One more filename change

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -1,7 +1,7 @@
 class Relay < Formula
   desc "Cog's ChatOps command executor"
   homepage "https://github.com/operable/go-relay"
-  url "https://github.com/operable/go-relay/releases/download/0.4.0-alpha/cog-relay_darwin_amd64.tar.gz"
+  url "https://github.com/operable/go-relay/releases/download/0.4.0-alpha/relay_darwin_amd64.tar.gz"
   version "0.4.0-alpha"
   sha256 "f266491540edcd2d431f0321450cd142d52d65b38d22757b42c8ecfc65c9345d"
 
